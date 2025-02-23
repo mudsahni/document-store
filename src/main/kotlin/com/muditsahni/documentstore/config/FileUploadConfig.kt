dc0f6b84ef19221062151af3a6753ef695141d06
mudsahni <muditsahni@msn.com>
2025-02-24 00:09:08 +0530

    updates

--- src/main/kotlin/com/muditsahni/documentstore/config/FileUploadConfig.kt
+++ src/main/kotlin/com/muditsahni/documentstore/config/FileUploadConfig.kt
@@ -7,6 +7,6 @@ class FileUploadConfig {
     companion object {
         const val MAX_FILES = 25
         const val MAX_FILE_SIZE = 10 * 1024 * 1024L // 10MB in bytes
-        val ALLOWED_CONTENT_TYPES = setOf("application/pdf")
+        val ALLOWED_CONTENT_TYPES = setOf("application/pdf", "image/jpeg", "image/png", "image/tiff")
     }
 }
